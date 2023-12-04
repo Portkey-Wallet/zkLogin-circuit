987bd0fc43f9afe1306c5e89f2b22fb0b2aefd5e
Loong Yong En <loong@portkey.finance>
2023-12-04 12:22:33 +0800

    feat: add main circuit and tests

--- circuits/main.circom
+++ circuits/main.circom
@@ -3,24 +3,23 @@ include "./helpers/jwt.circom";
 include "./helpers/guardian-identifier-hash.circom";
 
 template Main(){
-  // verify that the JWT is valid
   component VERIFYJWT = JWTVerify(512, 121, 17);
-  signal input jwt;       // JWT
-  signal input signature; // Signature
-  signal input pubkey;    // Public key
+  signal input jwt[512];
+  signal input signature[17];
+  signal input pubkey[17];
 
-  VERIFYJWT.jwt = jwt;
-  VERIFYJWT.signature = signature;
-  VERIFYJWT.pubkey = pubkey;
+  VERIFYJWT.jwt <== jwt;
+  VERIFYJWT.signature <== signature;
+  VERIFYJWT.pubkey <== pubkey;
 
-  signal input sub;
-  signal input salt;
-  component HASH = GuardianIdentifierHash(512);
-  HASH.sub = sub;
-  HASH.salt = salt;
+  signal input sub[512];
+  signal input salt[512]; // public
+  component HASH = GuardianIdentifierHash(512, 512);
+  HASH.sub <== sub;
+  HASH.salt <== salt;
 
   signal output out[256];
-  out <== HASH.out;
+  out <== HASH.hash;
 }
 
-component main { public [ signature, pubkey ] } = Main();
+component main = Main();
---
+++ tests/main.test.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+import path from "path";
+import { pki } from "node-forge";
+import { wasm as wasm_tester } from "circom_tester";
+import { padString, toCircomBigIntBytes } from "./utils";
+import { describe, beforeAll, it } from "vitest";
+
+describe("Main Test", () => {
+  let circuit: any;
+
+  describe("Main", () => {
+    beforeAll(async () => {
+      circuit = await wasm_tester(
+        path.join(__dirname, "../circuits/main.circom"),
+        {
+          // @dev During development recompile can be set to false if you are only making changes in the tests.
+          // This will save time by not recompiling the circuit every time.
+          // Compile: circom "./tests/email-verifier-test.circom" --r1cs --wasm --sym --c --wat --output "./tests/compiled-test-circuit"
+          recompile: true,
+          output: path.join(__dirname, "./compiled-test-circuit"),
+          include: path.join(__dirname, "../node_modules"),
+        }
+      );
+    });
+
+    it("should main be ok", async function () {
+      // signature
+      const jwtSignature =
+        "NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ";
+      // eslint-disable-next-line prettier/prettier, no-restricted-globals
+      const signatureBigInt = BigInt(
+        "0x" + Buffer.from(jwtSignature, "base64").toString("hex")
+      );
+
+      // public key
+      const publicKeyPem = fs.readFileSync(
+        path.join(__dirname, "./keys/public_key.pem"),
+        "utf8"
+      );
+      const pubKeyData = pki.publicKeyFromPem(publicKeyPem.toString());
+      const pubkeyBigInt = BigInt(pubKeyData.n.toString());
+
+      const startTime = new Date().getTime();
+      const witness = await circuit.calculateWitness({
+        jwt: padString(
+          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0",
+          512
+        ),
+        signature: toCircomBigIntBytes(signatureBigInt),
+        pubkey: toCircomBigIntBytes(pubkeyBigInt),
+        sub: padString("116111375152810828167", 512),
+        salt: padString("a677999396dc49a28ad6c9c242719bb3", 512),
+      });
+      console.log("duration:", new Date().getTime() - startTime);
+
+      await circuit.checkConstraints(witness);
+
+      // TODO: prepare test case
+      // await circuit.assertOut(witness, { out: "" });
+    });
+  });
+});
