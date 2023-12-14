1acd73aeb050c8491fa53bd4f7447c548610111f
Loong Yong En <loong@portkey.finance>
2023-12-14 17:59:57 +0800

    feat: add video

--- src/generate-input.ts
+++ src/generate-input.ts
@@ -6,7 +6,6 @@ import { padString, toCircomBigIntBytes } from "../utils";
 export const generateInput = ({
   jwtSignature = "NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ",
   jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0",
-  sub = "116111375152810828167",
   salt = "a677999396dc49a28ad6c9c242719bb3",
 }) => {
   // eslint-disable-next-line prettier/prettier, no-restricted-globals
@@ -27,9 +26,21 @@ export const generateInput = ({
     jwt: padString(jwt, 512),
     signature: toCircomBigIntBytes(signatureBigInt),
     pubkey: toCircomBigIntBytes(pubkeyBigInt),
-    sub: padString(sub, 512),
-    salt: padString(salt, 512),
+    salt: padString(salt, 32),
   };
 
   return input;
 };
+
+const output = generateInput({});
+
+fs.writeFile(
+  "out/guardianhash_js/input.json",
+  JSON.stringify(output),
+  (err) => {
+    if (err) {
+      console.error(err);
+    }
+    // done!
+  }
+);
--- utils/hex-to-bytes.ts
+++ utils/hex-to-bytes.ts
@@ -1,4 +1,4 @@
-export function hexToBytes(hex) {
+export function hexToBytes(hex: string) {
   let bytes = [];
   for (let c = 0; c < hex.length; c += 2)
     bytes.push(parseInt(hex.substr(c, 2), 16));
