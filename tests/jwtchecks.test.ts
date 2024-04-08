02fef70bcf7a5fed2aec2e138fb8d3acca56cde0
gldeng <realgldeng@gmail.com>
2024-04-08 19:33:33 +0800

    Add more tests for JwtChecks

--- tests/jwtchecks.test.ts
+++ tests/jwtchecks.test.ts
@@ -26,8 +26,72 @@ describe("JWT Checks Test", () => {
       );
     });
 
-    
     it("should JWT Checks be ok", async function () {
+      const data = {
+        content: padString(
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6IjkzNGE1ODE2NDY4Yjk1NzAzOTUzZDE0ZTlmMTVkZjVkMDlhNDAxZTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAxMTcyMDcxMTQyMjExMTU4NjgiLCJlbWFpbCI6InN0ZXZlbmRlbmc4NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IjBYRFlzLURESzNKdmR4blY5bnJxcEEiLCJuYW1lIjoiR3VhbmdsZWkgRGVuZyIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLVGZ6OFpPLS1TZGFjdzVMVFhlbVQxNllwTlNsQm9ncFIzSHEzdWJIQ0w9czk2LWMiLCJnaXZlbl9uYW1lIjoiR3VhbmdsZWkiLCJmYW1pbHlfbmFtZSI6IkRlbmciLCJpYXQiOjE3MTIxNDIyMzcsImV4cCI6MTcxMjE0NTgzN30",
+          2048
+        ),
+        index_b64: 103 + 265,
+        // index_b64: 198,
+        length_b64: 42,
+        payload_start_index: 103,
+
+        ext_claim: padString(
+          '"sub":"110117207114221115868",',
+          256
+        ), // correct
+        ext_claim_length: 30,
+        name_length: 5,
+        colon_index: 5,
+        value_index: 6,
+        value_length: 23,
+      }
+      
+      const witness = await circuit.calculateWitness(data);
+
+      await circuit.checkConstraints(witness);
+      const claim_name = '"sub"';
+      const claim_value = '"110117207114221115868"';
+      await circuit.assertOut(witness, {
+        claim_name: padString(claim_name, claim_name.length),
+        claim_value: padString(claim_value, claim_value.length)
+      });
+    });
+
+    it("should JWT Checks be ok - with curly brace ending", async function () {
+      const data = {
+        content: padString(
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6IjkzNGE1ODE2NDY4Yjk1NzAzOTUzZDE0ZTlmMTVkZjVkMDlhNDAxZTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAxMTcyMDcxMTQyMjExMTU4NjgiLCJlbWFpbCI6InN0ZXZlbmRlbmc4NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IjBYRFlzLURESzNKdmR4blY5bnJxcEEiLCJuYW1lIjoiR3VhbmdsZWkgRGVuZyIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLVGZ6OFpPLS1TZGFjdzVMVFhlbVQxNllwTlNsQm9ncFIzSHEzdWJIQ0w9czk2LWMiLCJnaXZlbl9uYW1lIjoiR3VhbmdsZWkiLCJmYW1pbHlfbmFtZSI6IkRlbmciLCJpYXQiOjE3MTIxNDIyMzcsImV4cCI6MTcxMjE0NTgzN30",
+          2048
+        ),
+        index_b64: 103 + 676,
+        // index_b64: 198,
+        length_b64: 23,
+        payload_start_index: 103,
+        ext_claim: padString(
+          '"exp":1712145837}',
+          256
+        ), // correct
+        ext_claim_length: 17,
+        name_length: 5,
+        colon_index: 5,
+        value_index: 6,
+        value_length: 10,
+      }
+      
+      const witness = await circuit.calculateWitness(data);
+
+      await circuit.checkConstraints(witness);
+      const claim_name = '"exp"';
+      const claim_value = '1712145837';
+      await circuit.assertOut(witness, {
+        claim_name: padString(claim_name, claim_name.length),
+        claim_value: padString(claim_value, claim_value.length)
+      });
+    });
+
+    it("should JWT Checks be ok - with comma prefix", async function () {
       const data = {
         content: padString(
           "eyJhbGciOiJSUzI1NiIsImtpZCI6IjkzNGE1ODE2NDY4Yjk1NzAzOTUzZDE0ZTlmMTVkZjVkMDlhNDAxZTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3MzcwMjgwNDA4NTgtOHVmcXNvYzdpNWtmc3NkdGt1N3Rtc2dzc25tOGZjOGQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAxMTcyMDcxMTQyMjExMTU4NjgiLCJlbWFpbCI6InN0ZXZlbmRlbmc4NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IjBYRFlzLURESzNKdmR4blY5bnJxcEEiLCJuYW1lIjoiR3VhbmdsZWkgRGVuZyIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLVGZ6OFpPLS1TZGFjdzVMVFhlbVQxNllwTlNsQm9ncFIzSHEzdWJIQ0w9czk2LWMiLCJnaXZlbl9uYW1lIjoiR3VhbmdsZWkiLCJmYW1pbHlfbmFtZSI6IkRlbmciLCJpYXQiOjE3MTIxNDIyMzcsImV4cCI6MTcxMjE0NTgzN30",
